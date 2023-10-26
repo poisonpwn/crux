1d0d88e569e597c06e3c7d0dd1b4719aeac6051e
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2023-10-26 18:48:37 +0530

    feat: fetch messages from discord
    fetch messages from discord until but not including the users's last message
    --
    OR
    --
    if there are replies within the fetched region to messages outside the
    region, then all the messages from the beginning to the earliest message
    that was replied to will be fetched.

---
+++ summary_bot/src/messages.py
@@ -0,0 +1,95 @@
+from collections import deque
+import datetime as dt
+from discord import DeletedReferencedMessage, User
+from discord.channel import TextChannel
+from discord.enums import MessageType
+
+
+class TimeRange:
+    def __init__(self, start_time, end_time):
+        if not start_time < end_time:
+            raise Exception("TimeRange has to be forward ordered")
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def __contains__(self, time):
+        return self.start_time <= time <= self.end_time
+
+
+class MessagesFetcher:
+    SEARCH_LIMIT = 300
+    ALLOWED_MESG_TYPES = [MessageType.default, MessageType.reply]
+
+    def __init__(self, channel: TextChannel, exclude_predicate):
+        self.channel = channel
+        self.exclude_predicate = exclude_predicate
+
+    async def init_message_queue(self):
+        self.messages_queue = deque()
+
+        async for mesg in self.channel.history(limit=300):
+            should_ignore = any(
+                [
+                    mesg.type not in self.ALLOWED_MESG_TYPES,
+                    self.exclude_predicate(mesg),
+                ]
+            )
+            # ignore any messages that dont match this condition
+            if should_ignore:
+                continue
+
+            self.messages_queue.appendleft(mesg)
+
+    def update_message_queue(self, mesg):
+        if self.messages_queue is None:
+            raise Exception("message queue has to be initialized first")
+        if self.exclude_predicate(mesg):
+            return
+
+        self.messages_queue.append(mesg)
+        self.messages_queue.popleft()
+
+    @staticmethod
+    def __time_range(queue):
+        return TimeRange(queue[0].created_at, queue[-1].created_at)
+
+    def get_summary_messages(self, query_user: User):
+        if self.messages_queue is None:
+            raise Exception("message queue has to be initialized first")
+        messages_queue = self.messages_queue.copy()
+        result_list = deque()
+        time_range = self.__time_range(messages_queue)
+        earl_reply_time = dt.datetime.now(dt.timezone.utc)  # max value for now
+        earl_reply_id = None
+
+        for back_index, mesg in enumerate(reversed(messages_queue), start=1):
+            if mesg.author == query_user:
+                author_last_mesg_ind = len(messages_queue) - back_index
+                break
+
+            if (
+                mesg.reference is not None
+                and (ref_mesg := mesg.reference.resolved)
+                is not DeletedReferencedMessage
+            ):
+                if ref_mesg.created_at not in time_range:
+                    raise Exception("Summary Limit Exceeded")
+
+                if ref_mesg.created_at < earl_reply_time:
+                    earl_reply_time = ref_mesg.created_at
+                    earl_reply_id = ref_mesg.id
+
+            result_list.appendleft(mesg)
+        else:
+            raise Exception("Summary Limit Exceeded")
+
+        if earl_reply_id is None:
+            return result_list
+
+        for index in reversed(range(author_last_mesg_ind + 1)):
+            mesg = messages_queue[index]
+            result_list.appendleft(mesg)
+            if mesg.id == earl_reply_id:
+                break
+
+        return result_list
