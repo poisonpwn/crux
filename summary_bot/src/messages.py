7296922b0740ae6ab254b2a41d94972028501c4c
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2023-10-27 19:34:50 +0530

    feat(message_fetcher): add range fetching capability
    extract the whole should_ignore any block into a should_include
      predicate property
    
    rename get_summary_messages to get_till_last_message
    
    limit message fetching to SEARCH_LIMIT ClassVar
    
    better exception handling using custom exception classes

--- summary_bot/src/messages.py
+++ summary_bot/src/messages.py
@@ -4,6 +4,11 @@ from discord import DeletedReferencedMessage, User
 from discord.channel import TextChannel
 from discord.enums import MessageType
 
+from exceptions import (
+    MessageQueueNotInitialized,
+    MessageFetchError,
+)
+
 
 class TimeRange:
     def __init__(self, start_time, end_time):
@@ -20,15 +25,11 @@ class Conversation:
     def __init__(self, messages):
         self.messages = messages
 
-    @staticmethod
-    def _sanitize(string: str):
-        return string.replace("\n", " ")
-
     def __str__(self):
         convo_lines = []
         for mesg in self.messages:
-            sanitized_content = self._sanitize(str(mesg.content))
-            convo_lines.append(f"{mesg.author.global_name}: {sanitized_content}")
+            message_content = str(mesg.content)
+            convo_lines.append(f"{mesg.author.global_name}: {message_content}")
         return "\n".join(convo_lines)
 
 
@@ -38,43 +39,89 @@ class MessagesFetcher:
 
     def __init__(self, channel: TextChannel, exclude_predicate):
         self.channel = channel
-        self.exclude_predicate = exclude_predicate
+        self.should_exclude = lambda mesg: any(
+            [
+                mesg.type not in self.ALLOWED_MESG_TYPES,
+                exclude_predicate(mesg),
+            ]
+        )
+        self.messages_queue = None
 
     async def init_message_queue(self):
         self.messages_queue = deque()
 
-        async for mesg in self.channel.history(limit=300):
-            should_ignore = any(
-                [
-                    mesg.type not in self.ALLOWED_MESG_TYPES,
-                    self.exclude_predicate(mesg),
-                ]
-            )
+        async for mesg in self.channel.history(limit=self.SEARCH_LIMIT):
             # ignore any messages that dont match this condition
-            if should_ignore:
+            if self.should_exclude(mesg):
                 continue
 
             self.messages_queue.appendleft(mesg)
 
     def update_message_queue(self, mesg):
         if self.messages_queue is None:
-            raise Exception("message queue has to be initialized first")
-        if self.exclude_predicate(mesg):
+            raise MessageQueueNotInitialized(
+                "message queue has to be initialized first"
+            )
+        if self.should_exclude(mesg):
             return
 
         self.messages_queue.append(mesg)
         self.messages_queue.popleft()
 
     @staticmethod
-    def __time_range(queue):
-        return TimeRange(queue[0].created_at, queue[-1].created_at)
+    def __messages_time_range(messages_queue) -> TimeRange:
+        """returns TimeRange between which the messaage queue spans
+        Args:
 
-    def get_summary_messages(self, query_user: User):
+          messages_queue: the messages queue for which the time range
+            is to be calculated.
+        """
+        return TimeRange(
+            messages_queue[0].created_at,
+            messages_queue[-1].created_at,
+        )
+
+    def get_message_range(self, *args) -> deque:
+        """
+        get last messages from the channel
+
+        Args:
+          *args: stop_num; or start_num, stop_num (1 based indexing from the end)
+
+        Returns:
+        last stop_num messages if only stop_num is given as args is [stop_num]
+        range of messagesstart_num to stop_num in 1 based indexing if args is [start_num, stop_num]
+        """
         if self.messages_queue is None:
             raise Exception("message queue has to be initialized first")
+
+        start = 1
+        match (len(args)):
+            case 1:
+                stop = args[0]
+            case 2:
+                start, stop = args
+            case _:
+                raise (Exception("function only takes 1 or 2 arguments"))
+
+        in_bounds = lambda index: 1 <= index <= len(self.messages_queue)
+        if not all([in_bounds(start), in_bounds(stop), start < stop]):
+            raise MessageFetchError(f"{start} to {stop}, not a valid message range,")
+
         messages_queue = self.messages_queue.copy()
         result_list = deque()
-        time_range = self.__time_range(messages_queue)
+        for i in range(start, stop + 1):
+            result_list.appendleft(messages_queue[-i])
+
+        return result_list
+
+    def get_till_last_message(self, query_user: User) -> deque:
+        if self.messages_queue is None:
+            raise Exception("message queue has to be initialized first")
+
+        messages_queue = self.messages_queue.copy()
+        result_list = deque()
+        time_range = self.__messages_time_range(messages_queue)
         earl_reply_time = dt.datetime.now(dt.timezone.utc)  # max value for now
         earl_reply_id = None
 
@@ -83,13 +130,16 @@ class MessagesFetcher:
                 author_last_mesg_ind = len(messages_queue) - back_index
                 break
 
-            if (
-                mesg.reference is not None
-                and (ref_mesg := mesg.reference.resolved)
-                is not DeletedReferencedMessage
-            ):
+            if mesg.reference is not None:
+                if isinstance(
+                    ref_mesg := mesg.reference.resolved, DeletedReferencedMessage
+                ):
+                    continue
+
                 if ref_mesg.created_at not in time_range:
-                    raise Exception("Summary Limit Exceeded")
+                    raise MessageFetchError(
+                        "region contains references to messages are outside search scope"
+                    )
 
                 if ref_mesg.created_at < earl_reply_time:
                     earl_reply_time = ref_mesg.created_at
@@ -97,7 +147,9 @@ class MessagesFetcher:
 
             result_list.appendleft(mesg)
         else:
-            raise Exception("Summary Limit Exceeded")
+            raise MessageFetchError(
+                f"{query_user.global_name}'s last message outside search scope."
+            )
 
         if earl_reply_id is None:
             return result_list
