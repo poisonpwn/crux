7cd6f5be155d4b96592e8b5541a146e3c0a069b6
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2023-10-27 19:56:42 +0530

    feat(bot): hooked up summarization and message fetching in main.py
    dispatch to diff functions based on num of arguments given to !sum
    sending errors as messages in the channel

--- summary_bot/src/bot.py
+++ summary_bot/src/bot.py
@@ -1,6 +1,18 @@
 import discord
+from exceptions import MessageFetchError
+from messages import MessagesFetcher, Conversation
 from dotenv import dotenv_values
+from summarize import Summarizer
 
+
+async def get_message_fetcher(channel, exclude_predicate):
+    message_fetcher = MessagesFetcher(
+        channel,
+        exclude_predicate,
+    )
+    await message_fetcher.init_message_queue()
+
+    return message_fetcher
 
 
 def run_discord_bot():
@@ -9,25 +21,54 @@ def run_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
+    message_fetchers = {}
+    summarizer = Summarizer()
+    print("Summarizer initialized")
+
+    def exclude_predicate(mesg):
+        return mesg.content.startswith(WAKE_WORD) or mesg.author == client.user
 
     @client.event
     async def on_ready():
         print(f"{client.user} is now ready!")
 
     @client.event
-    async def on_message(message):
-        if message.author == client.user:
+    async def on_message(mesg):
+        if channel_id := mesg.channel.id not in message_fetchers:
+            message_fetchers[channel_id] = await get_message_fetcher(
+                mesg.channel, exclude_predicate
+            )
+
+        message_fetcher = message_fetchers[channel_id]
+
+        if mesg.author == client.user:
             return
 
-        user_message = str(message.content)
-        if not user_message.startswith(WAKE_WORD):
+        first_word, *fetch_bounds = str(mesg.content).split(" ")
+        if first_word != WAKE_WORD:
             return
 
-        user_message = user_message[len(WAKE_WORD) + 1 :]
-        print(user_message)
+        try:
+            if fetch_bounds:
+                conversation_messages = message_fetcher.get_message_range(
+                    *[int(bound) for bound in fetch_bounds]
+                )
+            else:
+                conversation_messages = message_fetcher.get_till_last_message(
+                    mesg.author
+                )
+        except MessageFetchError as e:
+            await mesg.channel.send(str(e))
+            return
 
-        if user_message == "hello":
-            response = "Hi"
-            await message.channel.send(response)
+        conversation = Conversation(conversation_messages)
+        if not conversation:
+            return
+
+        summary = summarizer.summarize(str(conversation))
+        try:
+            await mesg.author.send(summary)
+        except discord.errors.Forbidden:
+            await mesg.channel.send("messaging not allowed")
 
     client.run(TOKEN)
